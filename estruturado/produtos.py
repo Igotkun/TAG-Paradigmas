1bc2fdbb5c86689f37b1959addffdcb7a9d589de
Igotkun <igorclyra1@gmail.com>
2025-12-07 19:24:18 -0300

    produtos

--- estruturado/produtos.py
+++ estruturado/produtos.py
@@ -1,14 +1,36 @@
 # Aqui ficará o estoque (persistência)
 # Também aqui ficam funções estruturadas
 
+from oo.produto import Produto
+
+
+carrinho = [] 
+lista_produtos = []
+
+notebook = Produto("Notebook", 3500.00, "Eletrônicos", 5)
+leite = Produto("Leite", 5.90, "Alimentos", 50)
+livro = Produto("O senhor dos Anéis", 89.90, "Livros", 15)
+
+lista_produtos.append(notebook)
+lista_produtos.append(leite)
+lista_produtos.append(livro)
+
 def carregar_estoque():
     pass
 
 def salvar_estoque(estoque):
     pass
 
-def listarProdutos():
-    pass
+def listarProdutos(lista_produtos):
+    print("\n## CATÁLOGO DE PRODUTOS ##")
+    if not lista_produtos:
+        print("Nenhum produto cadastrado")
+        return
+    
+    for i, produto in enumerate(lista_produtos):
+        print(f"[{i + 1}] {produto}")
+
+    print("-" * 30)
 
 def alterarEstoque(codigo, nova_qtd):
     pass
